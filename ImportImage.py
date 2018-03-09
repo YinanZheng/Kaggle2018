bf72cabb981fe7f8c9279adc2d54cfe38f166521
YinanZheng <y-zheng@northwestern.edu>
2018-03-08 20:55:28 -0600

    Import image

--- ImportImage.py
+++ ImportImage.py
@@ -1,18 +1,25 @@
 import os
 import random
 import warnings
-
 import numpy as np
 import sys
-from skimage.io import imread
+import matplotlib.pyplot as plt
+
 from skimage.transform import resize
 from tqdm import tqdm
+from skimage.io import imread, imshow, imread_collection, concatenate_images
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
 
 # Set some parameters
 IMG_WIDTH = 128
 IMG_HEIGHT = 128
 IMG_CHANNELS = 3
-TRAIN_PATH = './input/stage1_train/'
+TRAIN_PATH = './input/stage1_train_small/'
 TEST_PATH = './input/stage1_test/'
 
 warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
@@ -60,3 +67,22 @@ for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
 print('Done!')
 
 # Check
+
+# Check if training data looks all right
+ix = random.randint(0, len(train_ids))
+imshow(X_train[ix])
+plt.show()
+imshow(np.squeeze(Y_train[ix]))
+plt.show()
+
+# check the size of dataset
+m_train = X_train.shape[0]
+num_px = X_train.shape[1]
+m_test = X_test.shape[0]
+
+print ("Number of training examples: " + str(m_train))
+print ("Number of testing examples: " + str(m_test))
+print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print ("train_x_orig shape: " + str(X_train.shape))
+print ("train_y shape: " + str(Y_train.shape))
+print ("test_x_orig shape: " + str(X_test.shape))
